375c1633188b261bc917dc318edc355485cfa1f7
Ted van der Voorde <ted.van.der.voorde@bumastemra.nl>
2019-02-20 15:05:51 +0100

    test

--- test/web/test.sh
+++ test/web/test.sh
@@ -2,23 +2,23 @@ curl $1|grep DEVOPS
 if [ $? -ne 0 ]
 then
         error=1
-        echo "<?xml version='1.0'?>" > web.junit.xml
-        echo "<testsuites>" >> web.junit.xml
-        echo "<testsuite name='web-server' tests='1' failed='1' failures='1'>" >> web.junit.xml
-        echo "<testcase name='curl' classname='web-server.Anonymous' target='local://' time='1.0'>" >> web.junit.xml
-        echo "<failure message='expected INFRA'/>" >> web.junit.xml
-        echo "</testcase>" >> web.junit.xml
-        echo "</testsuite>" >> web.junit.xml
-        echo "</testsuites>" >> web.junit.xml
+        echo "<?xml version='1.0'?>" > TEST-web.junit.xml
+        echo "<testsuites>" >> TEST-web.junit.xml
+        echo "<testsuite name='web-server' tests='1' failed='1' failures='1'>" >> TEST-web.junit.xml
+        echo "<testcase name='curl' classname='web-server.Anonymous' target='local://' time='1.0'>" >> TEST-web.junit.xml
+        echo "<failure message='expected INFRA'/>" >> TEST-web.junit.xml
+        echo "</testcase>" >> TEST-web.junit.xml
+        echo "</testsuite>" >> TEST-web.junit.xml
+        echo "</testsuites>" >> TEST-web.junit.xml
 else
         error=0
-        echo "<?xml version='1.0'?>" > web.junit.xml
-        echo "<testsuites>" >> web.junit.xml
-        echo "<testsuite name='web-server' tests='1' failed='0' failures='0'>" >> web.junit.xml
-        echo "<testcase name='curl' classname='web-server.Anonymous' target='local://' time='1.0'>" >> web.junit.xml
-        echo "</testcase>" >> web.junit.xml
-        echo "</testsuite>" >> web.junit.xml
-        echo "</testsuites>" >> web.junit.xml
+        echo "<?xml version='1.0'?>" > TEST-web.junit.xml
+        echo "<testsuites>" >> TEST-web.junit.xml
+        echo "<testsuite name='web-server' tests='1' failed='0' failures='0'>" >> TEST-web.junit.xml
+        echo "<testcase name='curl' classname='web-server.Anonymous' target='local://' time='1.0'>" >> TEST-web.junit.xml
+        echo "</testcase>" >> TEST-web.junit.xml
+        echo "</testsuite>" >> TEST-web.junit.xml
+        echo "</testsuites>" >> TEST-web.junit.xml
 fi
 exit ${error}
 
